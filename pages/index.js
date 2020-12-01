47b794c7d3b8b2aa0d8bbd3ad6b32faac7017cd4
Felipe-casa <felipe_sth@hotmail.com>
2020-12-01 10:14:29 -0300

    Network Teste

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,44 @@
+import api from '../src/services/api';
+import {FiLogIn} from 'react-icons/fi'
+import { useRouter } from 'next/router'
+
+
 const { useState } = require("react");
 
 function Home(){
-    return <div>
-        <h1>Home 3</h1>
-        <Contador />
-    </div>
-}
+    const [id, setId] = useState('');
+    const router = useRouter()
 
-function Contador(){
-    const [contador, setContador] = useState(1);
-
-    function addContador(){
-        setContador(contador + 1);
+    async function handleLogin(e){
+        e.preventDefault();
+        try {
+            const response = await api.post('sessions', {id});
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name)
+            router.push('profile');
+        } catch (err) {
+            alert('Falha no login ' + err.message);
+        }
     }
     return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={addContador}>Adicionar</button>
+        <div className="logon-container">
+            <section className="form">
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu login</h1>
+                        <input placeholder="Sua id"
+                            value={id}
+                            onChange={e => setId(e.target.value)}
+                        />
+                        <button className="button" type="submit">Entrar</button>
+                        <a href="/register" className="back-link">
+                            <FiLogIn size={16} color="#E02041" />
+                            Não tenho cadastro.
+                        </a>
+                </form>                
+            </section>
         </div>
     )
 }
+
 
 export default Home;
