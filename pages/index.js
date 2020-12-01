d2abf0679d8fcd5a4e9ce9730c495c666ca52634
Felipe-casa <felipe_sth@hotmail.com>
2020-12-01 09:16:38 -0300

    Teste inicial

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+const { useState } = require("react");
+
+function Home(){
+    return <div>
+        <h1>Home</h1>
+        <Contador />
+    </div>
+}
+
+function Contador(){
+    const [contador, setContador] = useState(1);
+
+    function addContador(){
+        setContador(contador + 1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={addContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home;
